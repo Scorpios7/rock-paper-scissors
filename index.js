e8628526d31e9f7c6745619ce7abcfec3bfa9cfc
LIM <1134261085@qq.com>
2024-02-16 20:44:07 +0800

    rock-paper-scissors first version

---
+++ index.js
@@ -0,0 +1,62 @@
+const mapResult = ['rock', 'paper', 'scissors'];
+let myResult;
+let computerResult;
+let win = 0, loss = 0, tie = 0;
+
+function moveRock() {
+    myResult = 0;
+    computerResult = Math.floor(Math.random() * 3);
+    judge();
+}
+
+function movePaper() {
+    myResult = 1;
+    computerResult = Math.floor(Math.random() * 3);
+    judge();
+}
+
+function moveScissors() {
+    myResult = 2;
+    computerResult = Math.floor(Math.random() * 3);
+    judge();
+}
+
+function judge() {
+    const resultObj = document.querySelector('.result');
+    const myResultObj = document.getElementById('my-result');
+    const computerResultObj = document.getElementById('computer-result');
+    changeMoveImg(myResult, myResultObj);
+    changeMoveImg(computerResult, computerResultObj);
+    if (myResult === computerResult) {
+        tie++;
+        resultObj.innerHTML = 'Tie.';
+        document.getElementById('tie-count').innerHTML = tie;
+    } else if (computerResult === (myResult + 2) % 3) {
+        win++;
+        resultObj.innerHTML = 'Win.';
+        document.getElementById('win-count').innerHTML = win;
+    } else {
+        loss++;
+        resultObj.innerHTML = 'Loss.';
+        document.getElementById('loss-count').innerHTML = loss;
+    }
+}
+
+function changeMoveImg(result, obj) {
+    if (result === 0) {
+        obj.src = 'rock-emoji.png';
+    } else if (result === 1) {
+        obj.src = 'paper-emoji.png';
+    } else {
+        obj.src = 'scissors-emoji.png';
+    }
+}
+
+function reset() {
+    tie = 0;
+    win = 0;
+    loss = 0;
+    document.getElementById('loss-count').innerHTML = loss;
+    document.getElementById('win-count').innerHTML = win;
+    document.getElementById('tie-count').innerHTML = tie;
+}
